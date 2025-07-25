b60d1f1cdfeb0fab7a7fab5a87d85eae113ef137
RMIT-Ace <ace.authors@rmit.edu.au>
2025-07-25 12:47:51 +1000

    New xcode project

---
+++ prj/Numerology Calculator/Numerology Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Numerology Calculator
+//
+//  Created by Ace on 25/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ prj/Numerology Calculator/Numerology Calculator/Numerology_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Numerology_CalculatorApp.swift
+//  Numerology Calculator
+//
+//  Created by Ace on 25/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct Numerology_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
