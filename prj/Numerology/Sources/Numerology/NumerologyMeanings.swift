720006d6a7b69e567ca05c99249aaee0a72d5c7b
Ace <ace.authors@rmit.edu.au>
2025-08-05 16:47:17 +1000

    Refactored.

---
+++ prj/Numerology/Sources/Numerology/NumerologyMeanings.swift
@@ -0,0 +1,121 @@
+//
+//  File.swift
+//  Numerology
+//
+//  Created by Ace on 5/8/2025.
+//
+
+import Foundation
+
+/*
+ 
+ Sources:
+ 
+ 1. https://www.instyle.com/lifestyle/astrology/numerology#:~:text=Perhaps%20you've%20found%20that,and%20meaning%20of%20each%20number.
+ 
+ 2. https://www.numerology.com/articles/about-numerology/single-digit-numbers-in-numerology/
+ 
+ 3. https://www.instagram.com/reel/C8a5sHuoJYF/
+ 
+ */
+
+public let numberMeanings: [Int: String] = [
+    0: """
+       ???
+       """,
+    1: """
+       Originality. Leadership. Create own path. Confidence. Independent.
+       Goal-oriented. Innovative. Proactive. Pioneering.  Motivated.
+       Freedom-loving.
+       Friends: 1, 5, 7
+       
+       Forceful. Risky. Doubtful. Self-centered. Reckless.
+       Foes: 4, 6
+       """,
+    2: """
+       Balance. High intuitive. Establish relationship. Unifying. Influential.
+       Tactful. Sensitive. Coorporative. Inclusive. Supportive. Empathetic.
+       Friends: 2, 4, 8
+       
+       Indecisive. Easily hurt. Unassertive.
+       Foes: 5, 7
+       """,
+    3: """
+       Authenticity. Communication. Creative expression. Charming personality.
+       Artistic. Charming. Curious. Jovial. Youthful.
+       Friends: 3, 6, 9
+       
+       Naive. Unfocused. Shallow. Scattered. Optimistic. 
+       Foes: 4, 7, 8
+       """,
+    4: """
+       Stability. Pragmatic, sensible, organized. Analystical mind. Plan maker.
+       Loyal. Service-oriented. Strong. Traditional. Hard Worker. Patient.
+       Dependable.
+       Friends: 2, 4, 8
+       
+       Hard work, Stress. Dogmatic. Dull. Rigid. Strict.
+       Foes: 1, 3, 5, 9
+       """,
+    5: """
+       Freedom. Adventurous, spontaneous risk-taker. Free spirit. Love to travel.
+       Sales. Marketing. Curious. Adaptable. Social. Flexible. Energetic.
+       Independent.
+       Friends: 1, 5, 7
+       
+       Non-committal. Unreliable. Directionless. Unpredictable.
+       Restless. Inconsistent.
+       Foes: 2, 4, 6
+       """,
+    6: """
+       Acceptance. Artist. Design, beauty, fine arts, aesthetics, music.
+       Unconditional love. Supportive. Protective. Romantic. Caring. Healing.
+       Compassionate. Nurturing. Warm. Harmonious.
+       Friends: 3, 6, 9
+       
+       Passive. Self-sacrificing. Self-blaming. Idealistic.
+       Foes: 1, 5, 7
+       """,
+    7: """
+       Truth. Knowledge. Curiosity. Field expert. Spiritual. Analytical.
+       Teachers, trainers, advisors, consultants. Intellectual.
+       Mysterious. 
+       Friends: 1, 5, 7
+       
+       Nerdy. Skeptics. Reclusive. Secretive. Suspicious. Reserved.
+       Foes: 2, 3, 6, 8, 9
+       """,
+    8: """
+       Empowerment. Accomplishment in material realm. High position/incomes.
+       Balanced. Dedicated. Prosperous. Strong. Professional. Goal-oriented.
+       Friends: 2, 4, 8
+       
+       Take life too serious. Ambition. Materialistic. Authoritative. Entitled.
+       Foes: 3, 7, 9
+       """,
+    9: """
+       Awakening. Completion. Wisdom, understanding, and compassion. Humanitarians.
+       Tolerant. Supportive. Kind. Wise. Experienced. Spiritual. Aware.
+       Friends: 3, 6, 9
+       
+       Clinging. Resentful. Sacrificing. Suffering.
+       Foes: 4, 7, 8
+       """,
+    11: """
+       Seer. Powerful intuition. Deep sense of awareness about the things we cannot see.
+       Psychic abilities. Relationships. Teamwork. Can read people feelings.
+       Spiritual inner voice. Same as number 2 and much more.
+       
+       Burden if not learn how to handle deep sense of awareness.
+       """,
+    22: """
+       Master Builder. Intuition. Reality maker. Actualizers. Rational thinker. 
+       Great instinct. People skills. High energy. Anything is possible.
+       Same traits as number 4 and much more.
+       """,
+    33: """
+       Master Teacher. Pure love and light. Profound responsibility of guiding.
+       Healing and enlightment. Trusted by universe. Compassion. A channel for 
+       divine messages. It carries powers of the 11 and 22.
+       """,
+]
