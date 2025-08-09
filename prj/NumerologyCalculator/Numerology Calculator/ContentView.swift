014752c9e56eb1bc1ff71ea03d882ce46950729e
RMIT-Ace <ace.authors@rmit.edu.au>
2025-08-09 18:13:23 +1000

    Finalised

--- prj/NumerologyCalculator/Numerology Calculator/ContentView.swift
+++ prj/NumerologyCalculator/Numerology Calculator/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Numerology Calculator
 //
-//  Created by Ace on 25/7/2025.
+//  Created by Ace on 9/8/2025.
 //
 
 import SwiftUI
@@ -10,56 +10,27 @@ import Numerology
 
 struct ContentView: View {
     @Environment(NumerologyViewModel.self) var vm
-    
     var body: some View {
-        @Bindable var vm = vm
-        List {
-            HScrollSectionView($vm.digits) { digit in
-                UpDownNumberPickerView(digit: digit)
-            } header: {
-                Text("Inputs (\(vm.digits.count)):")
-            } footer: {
-                HStack {
-                    Spacer()
-                    RoundedCornerButton("C") { vm.resetAllDigits() }
-                    RoundedCornerButton("+") { vm.maxDigits += 1 }
-                    RoundedCornerButton("-") { vm.maxDigits -= 1 }
+        NavigationView {
+            VStack {
+                NavigationLink {
+                    NumerologyView()
+                } label: {
+                    Image("RMIT-logo")
+                        .resizable()
+                        .frame(width: 200, height: 200)
                 }
-            }
-            
-            Section {
-                HStack {
-                    Spacer()
-                    Text("\(vm.numerologyNumber)")
-                        .font(.system(size: 48))
-                        .bold()
-                    Spacer()
-                }
-            } header: {
-                Text("Numerology number:")
-            } footer: {
-                HStack {
-                    Spacer()
-                    Toggle("Reduce", isOn: $vm.isMasterNumberReduced)
-                        .toggleStyle(SwitchToggleStyle())
-                        .frame(width: 130)
-                }
-            }
-            
-            Section("Prediction") {
-                Text(vm.prediction)
-                    .font(.system(size: 20, weight: .bold, design: .default))
-                    .italic()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-            }
+                .buttonStyle(.plain)
 
+                Text("Welcome to HAPI!")
+                    .bold(true)
+            }
+            .padding()
         }
     }
-    
 }
 
 #Preview {
-    var vm = NumerologyViewModel()
     ContentView()
-        .environment(vm)
+        .environment(NumerologyViewModel())
 }
---
+++ prj/NumerologyCalculator/Numerology Calculator/NumerologyView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  Numerology Calculator
+//
+//  Created by Ace on 25/7/2025.
+//
+
+import SwiftUI
+import Numerology
+
+struct NumerologyView: View {
+    @Environment(NumerologyViewModel.self) var vm
+    
+    var body: some View {
+        @Bindable var vm = vm
+        List {
+            HScrollSectionView($vm.digits) { digit in
+                UpDownNumberPickerView(digit: digit)
+            } header: {
+                Text("Inputs (\(vm.digits.count)):")
+            } footer: {
+                HStack {
+                    Spacer()
+                    RoundedCornerButton("C") { vm.resetAllDigits() }
+                    RoundedCornerButton("+") { vm.maxDigits += 1 }
+                    RoundedCornerButton("-") { vm.maxDigits -= 1 }
+                }
+            }
+            
+            Section {
+                HStack {
+                    Spacer()
+                    Text("\(vm.numerologyNumber)")
+                        .font(.system(size: 48))
+                        .bold()
+                    Spacer()
+                }
+            } header: {
+                Text("Numerology number:")
+            } footer: {
+                HStack {
+                    Spacer()
+                    Toggle("Reduce", isOn: $vm.isMasterNumberReduced)
+                        .toggleStyle(SwitchToggleStyle())
+                        .frame(width: 130)
+                }
+            }
+            
+            Section("Prediction") {
+                Text(vm.prediction)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .italic()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+
+        }
+    }
+    
+}
+
+#Preview {
+    var vm = NumerologyViewModel()
+    NumerologyView()
+        .environment(vm)
+}
